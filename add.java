418a16f55d40cc2d8f8510f1a0570c5b42e1e0f5
slnarayana666 <slnarayana666@gmail.com>
2023-08-01 08:28:01 +0000

    Changes has been done

---
+++ add.java
@@ -0,0 +1,9 @@
+public class Main {
+  public static void main(String[] args) {
+    int x = 5;
+    int y = 6;
+    int sum = x + y;
+    System.out.println(sum); // Print the sum of x + y
+additon of two
+  }
+}
